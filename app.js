2f66c8bffe999a05f50b0db19fa0bb9fb0151a58
kunal <kunalbodkhe080@gmail.com>
2026-03-19 15:03:33 +0530

    fix port for render

--- app.js
+++ app.js
@@ -846,6 +846,8 @@ app.get("/logout", (req, res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000 on http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
