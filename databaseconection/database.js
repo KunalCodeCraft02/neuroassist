6384503356afb2cd69f1ec072bb8ecaecaa58b7c
kunal <kunalbodkhe080@gmail.com>
2026-03-19 20:59:08 +0530

    fix port for render

--- databaseconection/database.js
+++ databaseconection/database.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 require("dotenv").config()
 
+
+
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => {
-    console.log("Connected to MongoDB");
+
+    console.log("MongoDB Connected");
+
 })
 .catch((err) => {
-    console.error("Error connecting to MongoDB:", err);
+    console.log(" MongoDB Error:", err);
 });
 
 
