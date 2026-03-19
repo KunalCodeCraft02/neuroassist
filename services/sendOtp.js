65d86cf5fabd64448162b40d2d163aca5152a5e0
kunal <kunalbodkhe080@gmail.com>
2026-03-19 22:13:38 +0530

    fix port for render

--- services/sendOtp.js
+++ services/sendOtp.js
@@ -1,27 +1,35 @@
-const SibApiV3Sdk = require('sib-api-v3-sdk');
+const SibApiV3Sdk = require("sib-api-v3-sdk");
 
+// configure API key
 const client = SibApiV3Sdk.ApiClient.instance;
-const apiKey = client.authentications['api-key'];
-
+const apiKey = client.authentications["api-key"];
 apiKey.apiKey = process.env.BREVO_API_KEY;
 
 async function sendOtp(email, otp) {
+    try {
+        const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
-    const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+        const result = await apiInstance.sendTransacEmail({
+            sender: {
+                email: "hyperboy022@gmail.com", 
+                name: "NeuroAssist"
+            },
+            to: [{ email: email }],
+            subject: "Your OTP Code",
+            htmlContent: `
+                <h2>Email Verification</h2>
+                <p>Your OTP is:</p>
+                <h1>${otp}</h1>
+                <p>Expires in 5 minutes</p>
+            `
+        });
 
-    const sendSmtpEmail = {
-        to: [{ email: email }],
-        sender: { email: "hyperboy022@gmail.com", name: "NeuroAssist" },
-        subject: "Your OTP Code",
-        htmlContent: `
-            <h2>Email Verification</h2>
-            <p>Your OTP code is:</p>
-            <h1>${otp}</h1>
-            <p>This OTP will expire in 5 minutes.</p>
-        `
-    };
+        console.log("OTP SENT ", result.messageId);
 
-    await apiInstance.sendTransacEmail(sendSmtpEmail);
+    } catch (err) {
+        console.log("BREVO ERROR :", err.response?.body || err.message);
+        throw err;
+    }
 }
 
 module.exports = sendOtp;
